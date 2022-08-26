d847dbd93d39db211c87767b94b55923829af995
Maya Kappen <mayakappen@gmail.com>
2022-08-26 12:14:31 -0600

    Add styling for All-Movies and Movie-Card

--- src/All-Movies/All-Movies.js
+++ src/All-Movies/All-Movies.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MovieCard from '../Movie-Card/Movie-Card'
+import './All-Movies.css'
 
 const AllMovies = ({movies}) => {
     console.log(movies)
     const moviePosters = movies.map(movie => {
         return (
             <MovieCard
+                key={movie.id}
                 id={movie.id} 
                 title={movie.title}
                 poster={movie.poster_path}
