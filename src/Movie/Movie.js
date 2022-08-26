16b9f5c3749958fd6b6e529ad0aae394f519caa6
Anna Spitz <aspitzemail@gmail.com>
2022-08-25 20:32:11 -0500

    Add Movie component

---
+++ src/Movie/Movie.js
@@ -0,0 +1,39 @@
+import { getByTitle } from "@testing-library/react";
+import './Movie.css'
+
+const Movie = ({ movieDetails }) => {
+    const {
+        title, posterPath, backdropPath, releaseDate, overview,
+        genres, budget, revenue, runtime, tagline, averageRating
+    } = movieDetails;
+
+    const releaseDateFormatted = new Date(releaseDate).toLocaleDateString('en-US');
+    
+    return (
+        <div className='movie-container'>
+            <div className='movie-heading-container' 
+                style={{backgroundImage: `url(${backdropPath})`, 
+                backgroundSize: 'cover',
+                boxShadow: 'inset 30vw 10px 100px black'
+            }}>
+                <h2>{title}</h2>
+                <ul className="movieHeadingDetails">
+                    <li>Average Rating: {averageRating}</li>
+                    <li>{genres.join(' | ')} {releaseDateFormatted}</li>
+                    <li>{runtime} Minutes</li>
+                    <li>Budget: ${budget.toLocaleString('en-US')}</li>
+                    <li>Revenue: ${revenue.toLocaleString('en-US')}</li>
+                </ul>
+            </div>
+            <div className="movie-description-container">
+                <div>
+                    {tagline.length && <h3 className="tagline">{tagline}</h3>}
+                    <p>{overview}</p>
+                </div>
+                <img className='poster-movie-view' src={posterPath} />
+            </div>
+        </div>
+    )
+}
+
+export default Movie;
