395680285c599f379fcf0c7703e7a9169b7fa896
Maya Kappen <mayakappen@gmail.com>
2022-08-28 17:36:01 -0600

    Format overlay data

--- src/Movie-Card/Movie-Card.js
+++ src/Movie-Card/Movie-Card.js
@@ -2,14 +2,17 @@ import React from 'react'
 import './Movie-Card.css'
 const MovieCard = ({ movieDetails, switchView}) => {
     const {
-        title, posterPath, id, releaseDate, overview,
-        genres, budget, revenue, runtime, tagline, averageRating
+        title, poster_path, id, release_date, overview,
+        genres, budget, revenue, runtime, tagline, average_rating
     } = movieDetails;
-    return <article  key={id} style={{ backgroundImage: `url(${posterPath})`}} id={id} name={title} alt={title} className='movie-card' onClick={() => switchView(id)}>
+    
+    const releaseDate = new Date(release_date).toLocaleDateString('en-US')
+    const avgRating = average_rating.toFixed(1)
+    return <article  key={id} style={{ backgroundImage: `url(${poster_path})`}} id={id} name={title} alt={title} className='movie-card' onClick={() => switchView(id)}>
         <h3>{title}</h3>
         <ul>
             <li className='release'>{releaseDate}</li>
-            <li className='rating'>{averageRating}</li>
+            <li className='rating'>{avgRating}</li>
             <li className='genres'>{genres}</li>
         </ul>
     </article>
