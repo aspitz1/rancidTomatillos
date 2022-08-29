d7148679a098291e602a49f45751d427e4048736
Maya Kappen <mayakappen@gmail.com>
2022-08-28 21:21:51 -0600

    Finish styling for hover info

--- src/Movie-Card/Movie-Card.js
+++ src/Movie-Card/Movie-Card.js
@@ -2,19 +2,25 @@ import React from 'react'
 import './Movie-Card.css'
 const MovieCard = ({ movieDetails, switchView}) => {
     const {
-        title, poster_path, id, release_date, overview,
-        genres, budget, revenue, runtime, tagline, average_rating
+        title, poster_path, id, release_date, average_rating
     } = movieDetails;
-    
+
     const releaseDate = new Date(release_date).toLocaleDateString('en-US')
     const avgRating = average_rating.toFixed(1)
-    return <article  key={id} style={{ backgroundImage: `url(${poster_path})`}} id={id} name={title} alt={title} className='movie-card' onClick={() => switchView(id)}>
-        <h3>{title}</h3>
-        <ul>
-            <li className='release'>{releaseDate}</li>
-            <li className='rating'>{avgRating}</li>
-            <li className='genres'>{genres}</li>
-        </ul>
-    </article>
+    return <article  key={id} id={id} name={title} alt={title} className='movie-card' onClick={() => switchView(id)}>
+                <img className='background' src={poster_path} alt={title} height='300' width='200'></img>
+
+                <div className='hidden'>
+
+                    <h3>{title}</h3>
+                    <label for='release'>Released:
+                        <p className='text' id='release'>{releaseDate}</p>
+                    </label>
+                    <label for='rating'>Rating:
+                        <p className ='text' id='rating'>{avgRating}/10</p>
+                    </label>
+                </div>
+
+            </article>
 }
 export default MovieCard;
