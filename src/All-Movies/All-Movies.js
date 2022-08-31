120f47186639bf0051b7eedee1fabfc2a532c32b
Anna Spitz <aspitzemail@gmail.com>
2022-08-30 20:17:10 -0500

    Fix bugs after merge

--- src/All-Movies/All-Movies.js
+++ src/All-Movies/All-Movies.js
@@ -2,24 +2,18 @@ import React from 'react'
 import MovieCard from '../Movie-Card/Movie-Card'
 import './All-Movies.css'
 
-<<<<<<< HEAD
+
 const AllMovies = ({movies}) => {
-=======
-const AllMovies = ({movies, switchView, onHover}) => {
->>>>>>> main
+    console.log(movies);
     const moviePosters = movies.map(movie => {
         return (
             <MovieCard
                 key={movie.id}
-<<<<<<< HEAD
                 id={movie.id} 
                 title={movie.title}
                 poster={movie.poster_path}
-=======
-                movieDetails={movie}
-                switchView={switchView}
-                onHover={onHover} 
->>>>>>> main
+                averageRating={movie.average_rating}
+                releaseDate={movie.release_date}
             />
         )
     })
--- src/Movie-Card/Movie-Card.js
+++ src/Movie-Card/Movie-Card.js
@@ -2,20 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import './Movie-Card.css'
 
-const MovieCard = ({ movieDetails, switchView}) => {
-    const {
-        title, poster_path, id, release_date, average_rating
-    } = movieDetails;
-
-    const releaseDate = new Date(release_date).toLocaleDateString('en-US')
-    const avgRating = average_rating.toFixed(1)
-    const backgroundImage = poster_path;
-    return <Link to={'/' + id} key={id} id={id} name={title} alt={title} className='movie-card' onClick={() => switchView(id)} 
-    style={{height: '300px', width: '200px', backgroundImage: `url(${backgroundImage})`,backgroundRepeat: 'no-repeat',backgroundSize: 'contain',}}>
+const MovieCard = ({ key, id, title, poster, averageRating, releaseDate }) => {
+    const formattedReleaseDate = new Date(releaseDate).toLocaleDateString('en-US')
+    const avgRating = averageRating.toFixed(1)
+    return <Link to={'/' + id} id={id} name={title} alt={title} className='movie-card' 
+    style={{height: '300px', width: '200px', backgroundImage: `url(${poster})`,backgroundRepeat: 'no-repeat',backgroundSize: 'contain',}}>
                 <div className='hidden'>
                     <h3>{title}</h3>
                     <label>Released:
-                        <p className='text' id='release'>{releaseDate}</p>
+                        <p className='text' id='release'>{formattedReleaseDate}</p>
                     </label>
                     <label>Rating:
                         <p className ='text' id='rating'>{avgRating}/10</p>
