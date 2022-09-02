cf99202d15e37fcd614886873565e6e88cf8b5bd
Maya Kappen <mayakappen@gmail.com>
2022-09-02 15:45:21 -0600

    Add test for if there is a title

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
 describe('/App/App.js', () => {
+  beforeEach(() => {
+    cy.visit('localhost:3000')
+  })
   it('should visit ', () => {
     cy.visit('localhost:3000')
+  })
+  it('Should have a title', () => {
+    cy.get('nav').find('div').find('.heading').contains('Rancid Tomatillos')
   })
 })
